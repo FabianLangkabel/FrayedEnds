a5d3c1059b193b654f7089d39439438f23a4fa94
tscharfe <tscharfe@t-online.de>
2025-06-09 18:29:27 +0200

    minor changes

--- src/madpy/__init__.py
+++ src/madpy/__init__.py
@@ -2,3 +2,4 @@ from ._madpy_impl import RedirectOutput
 from ._madpy_impl import PNOInterface 
 from ._madpy_impl import Optimization
 from ._madpy_impl import PyFuncFactory
+from ._madpy_impl import CoulombPotentialFromChargeDensity
