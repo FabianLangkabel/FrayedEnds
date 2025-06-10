71a6a3e9c5ddf5010d6825f31da81e300356d88c
tscharfe <tscharfe@t-online.de>
2025-06-10 12:05:56 +0200

    added integrals class to this branch

---
+++ src/integrals.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "MadnessProcess.hpp"
+#include "functionsaver.hpp"
+#include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
+
+#include <iostream>
+#include <madness/mra/mra.h>
+#include <madness/mra/vmra.h>
+#include <madness/mra/operator.h>
+#include <madness/chem/oep.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <algorithm>
+
+using namespace madness;
+namespace nb = nanobind;
+
+class Integrals: public MadnessProcess{
+public:
+    Integrals(double L, long k, double thresh);
+    ~Integrals(){};
+
+    madness::Tensor<double> potential_integrals;
+    madness::Tensor<double> overlap_integrals;
+    madness::Tensor<double> kinetic_integrals;
+    madness::Tensor<double> two_body_integrals;
+
+    nb::ndarray<nb::numpy, double, nb::ndim<2> > compute_overlap_integrals(std::vector<SavedFct> all_orbs);
+    nb::ndarray<nb::numpy, double, nb::ndim<2> > compute_potential_integrals(std::vector<SavedFct> all_orbs, SavedFct potential);
+    nb::ndarray<nb::numpy, double, nb::ndim<2> > compute_kinetic_integrals(std::vector<SavedFct> all_orbs);
+    nb::ndarray<nb::numpy, double, nb::ndim<4> > compute_two_body_integrals(std::vector<SavedFct> all_orbs);
+
+    void hello(){std::cout << "hello from the integrals class\n";}
+};
