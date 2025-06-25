2346a5db44944f75b9e1cb7d06794cdcca3a9cf8
tscharfe <tscharfe@t-online.de>
2025-06-25 20:13:53 +0200

    .

--- examples/pno.py
+++ examples/pno.py
@@ -3,6 +3,25 @@ import madpy
 import tequila as tq
 from time import time
 
+
+def get_best_initial_values(mol):
+    tries = 20
+    U = mol.make_ansatz(name="HCB-UpCCGD")
+    best_opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess="random")
+    opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True)
+    if opt.energy < best_opt.energy:
+        best_opt = opt
+    
+    for _ in range(tries):
+        #opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess="random")
+        initial_guess = np.eye(mol.n_orbitals) + np.random.normal(scale=1.0, loc=0.0, size=mol.n_orbitals**2).reshape(mol.n_orbitals, mol.n_orbitals)
+        opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess=initial_guess)
+        if opt.energy < best_opt.energy:
+            best_opt = opt
+            
+    return best_opt
+
+
 true_start=time()
 start = time()
 # initialize the PNO interface
@@ -17,13 +36,20 @@ del madpno
 end=time()
 print("PNO time: ", end-start)
 
-for iteration in range(6):
+for iteration in range(1):
     start = time()
     mol = tq.Molecule(geometry=geom, one_body_integrals=h, two_body_integrals=g, nuclear_repulsion=c)
-    U = mol.make_ansatz(name="UpCCGD")
-    H = mol.make_hamiltonian()
+    U = mol.make_ansatz(name="HCB-UpCCGD")
+    if iteration == 0:
+        opt = get_best_initial_values(mol)
+    else:
+        opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess=opt.mo_coeff)
+    
+    mol_new = opt.molecule
+    H = mol_new.make_hardcore_boson_hamiltonian()
+    U = mol_new.make_ansatz(name="HCB-UpCCGD")
     E = tq.ExpectationValue(H=H, U=U)
-    result = tq.minimize(E, silent=True)
+    result = tq.minimize(E, silent=True, use_hcb=True)
     rdm1, rdm2 = mol.compute_rdms(U, variables=result.variables)
     end = time()
     print("VQE iteration {} time: {:.2f} seconds".format(iteration, end-start))
@@ -31,10 +57,10 @@ for iteration in range(6):
     print("iteration {} energy {:+2.5f}".format(iteration, result.energy))
     
     start = time()
-    opt = madpy.Optimization(param)
-    orbitals = opt(orbitals=orbitals, rdm1=rdm1, rdm2=rdm2)
-    c,h_el,g_el = opt.get_integrals()
-    del opt
+    opti = madpy.Optimization(param)
+    orbitals = opti(orbitals=orbitals, rdm1=rdm1, rdm2=rdm2)
+    c,h_el,g_el = opti.get_integrals()
+    del opti
 
     h=np.array(h_el).reshape(2,2)
     g=np.array(g_el).reshape(2,2,2,2)
