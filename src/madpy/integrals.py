8a6497ed7ef28c26a9f54cb25ef0d7f62258eedd
tscharfe <tscharfe@t-online.de>
2025-06-30 22:00:22 +0200

    changed integrator to ensure that the g tensors will come out with the desired ordering

--- src/madpy/integrals.py
+++ src/madpy/integrals.py
@@ -9,8 +9,12 @@ class Integrals(MadPyBase):
         super().__init__(*args, **kwargs)
         self.impl = IntegralsInterface(self.madness_parameters.L, self.madness_parameters.k, self.madness_parameters.thresh, self.madness_parameters.initial_level, self.madness_parameters.truncate_mode, self.madness_parameters.refine, self.madness_parameters.n_threads)
     def compute_two_body_integrals(self, orbitals, ordering="phys", *args, **kwargs):
-        g = self.impl.compute_two_body_integrals(orbitals)
-        return NBodyTensor(elems=g, ordering=ordering)
+        g_elems = self.impl.compute_two_body_integrals(orbitals)
+        g = NBodyTensor(elems=g_elems, ordering="phys")
+        if ordering != "phys":
+            return g.reorder(to=ordering)
+        else:
+            return g
 
     def compute_kinetic_integrals(self, orbitals, *args, **kwargs):
         return self.impl.compute_kinetic_integrals(orbitals)
