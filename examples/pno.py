2f4aebd9de53526ef14d788bd601f26e5ca91627
kottmanj <jakobkottmann@qalg033.informatik.uni-augsburg.de>
2025-07-08 15:54:09 +0200

    started to clean up pno code, wrote potentially unnecessary orbital datastructure

--- examples/pno.py
+++ examples/pno.py
@@ -1,5 +1,6 @@
 import numpy as np
 import madpy
+from madpy.parameters import unpack_madness_data
 import tequila as tq
 from time import time
 
@@ -9,6 +10,12 @@ true_start=time()
 geom = "H 0.0 0.0 -1.25\nH 0.0 0.0 1.25" # geometry in Angstrom
 madpno = madpy.MadPNO(geom, maxrank=1, pnoint={"n_pno":1})
 orbitals = madpno.get_orbitals(0,2,0)
+for i in range(len(orbitals)):
+    print(i)
+    print(orbitals[i].data)
+    print("info=",orbitals[i].data.info)
+    print("type=",orbitals[i].data.type)
+
 param = madpno.madness_parameters
 nuc_repulsion= madpno.get_nuclear_repulsion()
 Vnuc = madpno.get_nuclear_potential()
@@ -27,6 +34,7 @@ for iteration in range(6):
     T = integrals.compute_kinetic_integrals(orbitals)
     V = integrals.compute_potential_integrals(orbitals, Vnuc)
     S = integrals.compute_overlap_integrals(orbitals)
+    print(S)
     del integrals
 
     mol = tq.Molecule(geom, one_body_integrals=T+V, two_body_integrals=G, nuclear_repulsion=c)
@@ -36,13 +44,13 @@ for iteration in range(6):
     E = tq.ExpectationValue(H=H, U=U)
     result = tq.minimize(E, silent=True)
     rdm1, rdm2 = mol.compute_rdms(U, variables=result.variables)
-    
 
     print("iteration {} energy {:+2.5f}".format(iteration, result.energy))
     
     opti = madpy.Optimization(Vnuc, nuc_repulsion, parameters=param)
-    orbitals = opti.get_orbitals(orbitals=orbitals, rdm1=rdm1, rdm2=rdm2, opt_thresh=0.001, occ_thresh=0.001)
+    orbitals = opti.get_orbitals(orbitals=unpack_madness_data(orbitals), rdm1=rdm1, rdm2=rdm2, opt_thresh=0.001, occ_thresh=0.001)
     c = opti.get_c() #if there are no frozen core electrons, this should always be equal to the nuclear repulsion
+    print(orbitals)
     del opti
 
     plt=madpy.Plotter()
--- src/madpy/integrals.py
+++ src/madpy/integrals.py
@@ -1,5 +1,6 @@
 from ._madpy_impl import Integrals as IntegralsInterface
 from .baseclass import MadPyBase
+from .parameters import unpack_madness_data
 
 from tequila.quantumchemistry import NBodyTensor
 
@@ -9,7 +10,7 @@ class Integrals(MadPyBase):
         super().__init__(*args, **kwargs)
         self.impl = IntegralsInterface(self.madness_parameters.L, self.madness_parameters.k, self.madness_parameters.thresh, self.madness_parameters.initial_level, self.madness_parameters.truncate_mode, self.madness_parameters.refine, self.madness_parameters.n_threads)
     def compute_two_body_integrals(self, orbitals, ordering="phys", *args, **kwargs):
-        g_elems = self.impl.compute_two_body_integrals(orbitals)
+        g_elems = self.impl.compute_two_body_integrals(unpack_madness_data(orbitals))
         g = NBodyTensor(elems=g_elems, ordering="phys")
         if ordering != "phys":
             return g.reorder(to=ordering)
@@ -17,13 +18,13 @@ class Integrals(MadPyBase):
             return g
 
     def compute_kinetic_integrals(self, orbitals, *args, **kwargs):
-        return self.impl.compute_kinetic_integrals(orbitals)
+        return self.impl.compute_kinetic_integrals(unpack_madness_data(orbitals))
 
     def compute_potential_integrals(self, orbitals, Vnuc, *args, **kwargs):
-        return self.impl.compute_potential_integrals(orbitals, Vnuc)
+        return self.impl.compute_potential_integrals(unpack_madness_data(orbitals), unpack_madness_data(Vnuc))
 
     def compute_overlap_integrals(self, orbitals, *args, **kwargs):
-        return self.impl.compute_overlap_integrals(orbitals)
+        return self.impl.compute_overlap_integrals(unpack_madness_data(orbitals))
 
     def hello(self):
         self.impl.hello()
