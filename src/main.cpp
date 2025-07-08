51f95512c618e5dfdc65f6967c0370a18f3ca8b3
truonthu <thuy.truong@student.uni-augsburg.de>
2025-07-08 22:36:50 +0200

    added MadnessProcess to Nanobind

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,19 @@
 #include "PyFuncFactory.hpp"
 #include "integrals.hpp"
 #include "eigensolver.hpp"
+#include "MadnessProcess.hpp"
 
 namespace nb = nanobind;
 
 
 NB_MODULE(_madpy_impl, m) {
+    nb::class_<MadnessProcess>(m, "MadnessProcess")
+        .def(nb::init<const double &, const int &, const double &>())
+        .def("loadfct", &MadnessProcess::loadfct)
+        .def("loadfct_from_file", &MadnessProcess::loadfct_from_file)
+        .def("plot", &MadnessProcess::plot, nb::arg("filename"), nb::arg("f"), nb::arg("axis") = 2, nb::arg("datapoints") = 2001)
+        .def("plane_plot", &MadnessProcess::plane_plot, nb::arg("filename"), nb::arg("f"), nb::arg("plane") = "yz", nb::arg("zoom") = 1.0, nb::arg("datapoints") = 151, nb::arg("origin") = std::vector<double>({0.0, 0.0, 0.0}));
+
     nb::class_<real_function_3d>(m,"real_function_3d")
         .def(nb::init<>());
 
@@ -21,7 +29,7 @@ NB_MODULE(_madpy_impl, m) {
         .def_rw("type", &SavedFct::type);
 
     nb::class_<Integrals>(m, "Integrals")
-        .def(nb::init<const double &, const int &, const double &>())
+        .def(nb::init<MadnessProcess &>())
         .def("hello", &Integrals::hello)
         .def("compute_overlap_integrals", &Integrals::compute_overlap_integrals, nb::arg("all_orbs"))
         .def("compute_potential_integrals", &Integrals::compute_potential_integrals, nb::arg("all_orbs"), nb::arg("potential"))
@@ -29,11 +37,7 @@ NB_MODULE(_madpy_impl, m) {
         .def("compute_two_body_integrals", &Integrals::compute_two_body_integrals, nb::arg("all_orbs"));
 
     nb::class_<Optimization>(m, "Optimization")
-        .def(nb::init<const double &, const int &, const double &>())
-        .def("plot", &Optimization::plot, nb::arg("filename"), nb::arg("f"), nb::arg("axis") = 2, nb::arg("datapoints") = 2001)
-        .def("plane_plot", &Optimization::plane_plot, nb::arg("filename"), nb::arg("f"), nb::arg("plane") = "yz", nb::arg("zoom") = 1.0, nb::arg("datapoints") = 151, nb::arg("origin") = std::vector<double>({0.0, 0.0, 0.0}))
-        .def("loadfct", &Optimization::loadfct)
-        .def("loadfct_from_file", &Optimization::loadfct_from_file)
+        .def(nb::init<MadnessProcess &>())
         .def("GiveInitialOrbitals", &Optimization::GiveInitialOrbitals)
         .def("GiveRDMsAndRotateOrbitals", &Optimization::GiveRDMsAndRotateOrbitals)
         .def("CreateNuclearPotentialAndRepulsion", &Optimization::CreateNuclearPotentialAndRepulsion)
@@ -65,7 +69,7 @@ NB_MODULE(_madpy_impl, m) {
         .def_rw("BSH_eps", &Optimization::BSH_eps);
     
     nb::class_<PNOInterface>(m, "PNOInterface")
-        .def(nb::init<const std::string &, const double &, const int &, const double &>())
+        .def(nb::init<MadnessProcess &, const std::string &>())
         .def("DeterminePNOsAndIntegrals", &PNOInterface::DeterminePNOsAndIntegrals)
         .def("GetPNOs", &PNOInterface::GetPNOs)
         .def("GetHTensor", &PNOInterface::GetHTensor)
@@ -74,8 +78,7 @@ NB_MODULE(_madpy_impl, m) {
         .def("GetNuclearRepulsion", &PNOInterface::GetNuclearRepulsion);
 
     nb::class_<CoulombPotentialFromChargeDensity>(m, "CoulombPotentialFromChargeDensity")
-        .def(nb::init<const double &, const long &, const double &, const std::vector<double> &, const double &, const std::vector<std::vector<double> > &>())
-        .def("plot", &CoulombPotentialFromChargeDensity::plot, nb::arg("filename"), nb::arg("f"), nb::arg("axis") = 2, nb::arg("datapoints") = 2001)
+        .def(nb::init<MadnessProcess &, const std::vector<double> &, const double &, const std::vector<std::vector<double> > &>())
         .def("CreatePotential", &CoulombPotentialFromChargeDensity::CreatePotential)
         .def("CreateChargeDens", &CoulombPotentialFromChargeDensity::CreateChargeDens);
 
@@ -87,7 +90,7 @@ NB_MODULE(_madpy_impl, m) {
         .def("GetMRAFunction", &PyFuncFactory::GetMRAFunction);
 
     nb::class_<Eigensolver3D>(m, "Eigensolver")
-        .def(nb::init<const double &, const long &, const double &>())
+        .def(nb::init<MadnessProcess &>())
         .def("solve", &Eigensolver3D::solve, nb::arg("input_V"), nb::arg("num_levels"), nb::arg("max_iter"))
         .def("solve_with_guesses", &Eigensolver3D::solve_with_input_guesses, nb::arg("input_V"), nb::arg("input_guesses"), nb::arg("num_levels"), nb::arg("max_iter"))
         .def("GetOrbitals", &Eigensolver3D::GetOrbitals);
