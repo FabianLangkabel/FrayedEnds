8e12272376d3d86a43651f71e0bcc8ddb43a8ab8
kottmanj <jakob.kottmann@gmail.com>
2025-07-10 10:36:29 +0200

    further cleanup in pno (currently broken)

--- src/main.cpp
+++ src/main.cpp
@@ -53,8 +53,6 @@ NB_MODULE(_madpy_impl, m) {
         .def("GivePotentialAndRepulsion", &Optimization::GivePotentialAndRepulsion)
         .def("ReadInitialOrbitals", &Optimization::ReadInitialOrbitals)
         .def("ReadRDMFilesAndRotateOrbitals", &Optimization::ReadRDMFilesAndRotateOrbitals)
-        //.def("TransformMatrix", &Optimization::TransformMatrix)
-        //.def("TransformTensor", &Optimization::TransformTensor)
         .def("CalculateAllIntegrals", &Optimization::CalculateAllIntegrals)
         .def("CalculateCoreEnergy", &Optimization::CalculateCoreEnergy)
         .def("CalculateEnergies", &Optimization::CalculateEnergies)
@@ -80,7 +78,7 @@ NB_MODULE(_madpy_impl, m) {
     nb::class_<PNOInterface>(m, "PNOInterface")
         .def(nb::init<const std::string &, const double &, const int &, const double &, const int &, const int &, const bool &, const int &>())
         .def("get_nuclear_potential", &PNOInterface::get_nuclear_potential)
-        .def("DeterminePNOsAndIntegrals", &PNOInterface::DeterminePNOsAndIntegrals)
+        .def("DeterminePNOsAndIntegrals", &PNOInterface::run)
         .def("GetPNOs", &PNOInterface::GetPNOs)
         .def("GetNuclearRepulsion", &PNOInterface::GetNuclearRepulsion);
 
