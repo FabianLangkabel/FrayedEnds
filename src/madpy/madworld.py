da72ea50b03798e0bfcacb2b0f924b9997c431c4
Thuy <thuytruong112@gmail.com>
2025-07-18 19:28:27 +0200

    Add get_function_info function and updated MadWorld with read-only properties

--- src/madpy/madworld.py
+++ src/madpy/madworld.py
@@ -14,13 +14,33 @@ def redirect_output(filename="madness.out"):
         return wrapper
     return decorator
 
+def get_function_info(orbitals):
+    result = []
+    for x in orbitals:
+        info = {}
+        for kv in x.info.strip().split(" "):
+            kv = kv.split("=")
+            info[kv[0]]=eval(kv[1])
+        result.append({"type": x.type, **info})
+    return result
+
+
 class MadWorld:
     _impl = None
 
-    madness_parameters = {"L": 50.0, "k": 7, "thresh": 1.e-5, "initial_level": 5, "truncate_mode": 1, "refine": True,
-                          "n_threads": -1}
+    madness_parameters = {
+        "L": 50.0,
+        "k": 7,
+        "thresh": 1.e-5,
+        "initial_level": 5,
+        "truncate_mode": 1,
+        "refine": True,
+        "n_threads": -1
+    }
 
     def __init__(self, **kwargs):
+
+        self.madness_parameters = dict(self.madness_parameters)
 
         for k, v in kwargs.items():
             if k in self.madness_parameters:
@@ -35,6 +55,19 @@ class MadWorld:
                                     self.madness_parameters["truncate_mode"],
                                     self.madness_parameters["refine"],
                                     self.madness_parameters["n_threads"])
+
+    def __getattr__(self, name):
+        if name in self.madness_parameters:
+            return getattr(self._impl, name)
+        raise AttributeError(f"'MadWorld' object has no attribute '{name}'")
+
+    def __setattr__(self, name, value):
+        if hasattr(self, '_impl') and hasattr(self, 'madness_parameters') and name in self.madness_parameters:
+            raise AttributeError(f"Cannot modify read-only attribute '{name}'")
+        super().__setattr__(name, value)
+
+    def get_params(self):
+        return dict(self.madness_parameters)
 
     def line_plot(self, filename, mra_function, axis="z", datapoints=2001):
         self._impl.plot(filename, mra_function, axis, datapoints)
