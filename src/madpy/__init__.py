097d0d57ecf27f1828fd47581d9cf11880e96762
tscharfe <tscharfe@t-online.de>
2025-07-25 09:54:16 +0200

    extended molecule wrapper and renamed it and moved it to MadnessProcess

--- src/madpy/__init__.py
+++ src/madpy/__init__.py
@@ -1,11 +1,8 @@
 # todo: better integration
-from ._madpy_impl import (CoulombPotentialFromChargeDensity, NWChem_Converter,
-                          SavedFct, molecule)
+from ._madpy_impl import (CoulombPotentialFromChargeDensity, MadMolecule,
+                          NWChem_Converter, SavedFct)
 from .eigensolver import Eigensolver
 from .integrals import Integrals
 from .madpno import MadPNO
 from .madworld import MadWorld, get_function_info
 from .minbas import AtomicBasisProjector
-from .mrafunctionfactory import MRAFunctionFactory
-from .optimization import Optimization, transform_rdms
-from .pyscf_interface import PySCFInterface
