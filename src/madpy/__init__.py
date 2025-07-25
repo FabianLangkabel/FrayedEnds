948733031edbd1c07303d5747bb2844ff26c6f92
Thuy <thuytruong112@gmail.com>
2025-07-25 14:06:22 +0200

    made python class for MadMolecule

--- src/madpy/__init__.py
+++ src/madpy/__init__.py
@@ -1,11 +1,11 @@
-# todo: better integration
-from ._madpy_impl import (CoulombPotentialFromChargeDensity, MadMolecule, SavedFct)
+from ._madpy_impl import CoulombPotentialFromChargeDensity, SavedFct
 from .eigensolver import Eigensolver
 from .integrals import Integrals
+from .madmolecule import MadMolecule
 from .madpno import MadPNO
 from .madworld import MadWorld, get_function_info
 from .minbas import AtomicBasisProjector
-from .nwchem_converter import NWChem_Converter
 from .mrafunctionfactory import MRAFunctionFactory
+from .nwchem_converter import NWChem_Converter
 from .optimization import Optimization, transform_rdms
 from .pyscf_interface import PySCFInterface
