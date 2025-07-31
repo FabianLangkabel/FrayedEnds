202258339bc3c92ad1b2451f6d43f898c602d103
tscharfe <tscharfe@t-online.de>
2025-07-31 18:50:17 +0200

    changed the units to angstrom

--- src/madmolecule.cpp
+++ src/madmolecule.cpp
@@ -18,26 +18,32 @@ std::string MadMolecule::to_json() {
     return mol.to_json().dump();
 }
 
-// change the units in the following to Angstrom!!
+
 SavedFct MadMolecule::compute_nuclear_derivative(MadnessProcess& mp, const int atom, const int axis) {
+    double scale = 1e-10 / madness::constants::atomic_unit_of_length;
     madchem::MolecularDerivativeFunctor func(mol, atom, axis);
-    real_function_3d op = real_factory_3d(*(mp.world)).functor(func).nofence().truncate_on_project().truncate_mode(0);
+    real_function_3d op = real_factory_3d(*(mp.world)).functor(func).truncate_on_project().truncate_mode(0);
+    op = scale * op; // scale the function to Angstrom
     return SavedFct(op);
 }
 
 SavedFct MadMolecule::compute_second_nuclear_derivative(MadnessProcess& mp, const int atom, const int axis1,
                                                   const int axis2) {
+    double scale = 1e-10 / madness::constants::atomic_unit_of_length;                                     
     madchem::MolecularSecondDerivativeFunctor func(mol, atom, axis1, axis2);
-    real_function_3d op = real_factory_3d(*(mp.world)).functor(func).nofence().truncate_on_project().truncate_mode(0);
+    real_function_3d op = real_factory_3d(*(mp.world)).functor(func).truncate_on_project().truncate_mode(0);
+    op = scale * scale * op; // scale the function to Angstrom
     return SavedFct(op);
 }
 
 double MadMolecule::nuclear_repulsion_derivative(const int atom, const int axis) {
-    return mol.nuclear_repulsion_derivative(atom, axis);
+    double scale = 1e-10 / madness::constants::atomic_unit_of_length;
+    return scale * mol.nuclear_repulsion_derivative(atom, axis);
 }
 
 double MadMolecule::nuclear_repulsion_second_derivative(const int atom1, const int atom2, const int axis1, const int axis2) {
-    return mol.nuclear_repulsion_second_derivative(atom1, atom2, axis1, axis2);
+    double scale = 1e-10 / madness::constants::atomic_unit_of_length;
+    return scale * scale * mol.nuclear_repulsion_second_derivative(atom1, atom2, axis1, axis2);
 }
 
 
