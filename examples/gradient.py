88d608b1fb39eaf71ed207cbef54dd99ae967fca
tscharfe <tscharfe@t-online.de>
2025-08-18 11:21:50 +0200

    gradient test

--- examples/gradient.py
+++ examples/gradient.py
@@ -6,7 +6,7 @@ import time
 
 world = madpy.MadWorld()
 
-distance_list = [0.74+0.005 * i for i in range(2)]
+distance_list = [0.74+0.005 * i for i in range(1)]
 Energy_list=[]
 Gradient_list=[]
 
@@ -17,8 +17,8 @@ for distance in distance_list:
     molecule=madpy.MadMolecule()
     molecule.add_atom(0.0,0.0,0.0,"H")
     molecule.add_atom(0.0,0.0,distance,"H")
-
-    madpno = madpy.MadPNO(world, geometry, n_orbitals=8)
+    pno_start = time.time()
+    madpno = madpy.MadPNO(world, geometry, n_orbitals=2)
     orbitals = madpno.get_orbitals()
 
     nuc_repulsion = madpno.get_nuclear_repulsion()
@@ -29,6 +29,7 @@ for distance in distance_list:
 
 
     c = nuc_repulsion
+    current=0.0
     for iteration in range(6):
 
         integrals = madpy.Integrals(world)
@@ -46,22 +47,10 @@ for distance in distance_list:
         rdm1, rdm2 = mol.compute_rdms(U, variables=result.variables)
 
         print("iteration {} energy {:+2.7f}".format(iteration, result.energy))
+        if abs(result.energy-current)<1e-6:
+            break
+        current = result.energy
         
-        
-        if iteration == 5:
-            Energy_list.append(result.energy)
-            #gradient calculation
-            part_deriv_V = molecule.compute_nuclear_derivative(world, 1, 2)
-            Deriv_tens = integrals.compute_potential_integrals(orbitals, part_deriv_V)
-            part_deriv_c = molecule.nuclear_repulsion_derivative(1, 2)
-
-            grad=0.0
-            for i in range(len(orbitals)):
-                for j in range(len(orbitals)):
-                    grad += rdm1[i, j] * Deriv_tens[i, j]
-
-            print("gradient: ", grad+part_deriv_c)
-            Gradient_list.append(grad+part_deriv_c)
 
         opti = madpy.Optimization(world, Vnuc, nuc_repulsion)
         orbitals = opti.get_orbitals(orbitals=orbitals, rdm1=rdm1, rdm2=rdm2, opt_thresh=0.001, occ_thresh=0.001)
@@ -69,7 +58,20 @@ for distance in distance_list:
 
         for i in range(len(orbitals)):
             world.line_plot(f"orb{i}.dat", orbitals[i])
+    Energy_list.append(result.energy)
+    #gradient calculation
+    part_deriv_V = molecule.compute_nuclear_derivative(world, 1, 2)
+    Deriv_tens = integrals.compute_potential_integrals(orbitals, part_deriv_V)
+    part_deriv_c = molecule.nuclear_repulsion_derivative(1, 2)
 
+    grad=0.0
+    for i in range(len(orbitals)):
+        for j in range(len(orbitals)):
+            grad += rdm1[i, j] * Deriv_tens[i, j]
+
+    print("gradient: ", grad+part_deriv_c)
+    Gradient_list.append(grad+part_deriv_c)
+    
     true_end = time.time()
     print("Total time: ", true_end - true_start)
 
