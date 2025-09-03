a5ffed905ac54c685fc50b3a92c2f8e2c359bdfb
tscharfe <tscharfe@t-online.de>
2025-09-03 18:21:57 +0200

    wslehwsdas

--- examples/gradient/gradient_LiH.py
+++ examples/gradient/gradient_LiH.py
@@ -4,9 +4,73 @@ import tequila as tq
 import time
 
 
-world = madpy.MadWorld3D()
+def get_best_initial_values(mol):
+    tries = 20
+    U = mol.make_ansatz(name="HCB-UpCCGD")
+    best_opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess="random")
+    opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True)
+    if opt.energy < best_opt.energy:
+        best_opt = opt
+    
+    for _ in range(tries):
+        #opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess="random")
+        initial_guess = np.eye(mol.n_orbitals) + np.random.normal(scale=1.0, loc=0.0, size=mol.n_orbitals**2).reshape(mol.n_orbitals, mol.n_orbitals)
+        opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess=initial_guess)
+        if opt.energy < best_opt.energy:
+            best_opt = opt
+            
+    return best_opt
 
-distance_list = [30.0+0.025 * i for i in range(1)]
+def transform_rdms(TransformationMatrix, rdm1, rdm2):
+    new_rdm1 = np.dot(np.dot(TransformationMatrix.transpose(), rdm1),TransformationMatrix)
+    n = rdm2.shape[0]
+    
+    temp1 = np.zeros(shape=(n, n, n, n))
+    for i in range(n):
+        for j in range(n):
+            for k2 in range(n):
+                for l in range(n):
+                    k_value = 0
+                    for k in range(n):
+                        k_value += TransformationMatrix[k][k2] * rdm2[i][j][k][l]
+                    temp1[i][j][k2][l] = k_value
+                    
+    temp2 = np.zeros(shape=(n, n, n, n))
+    for i2 in range(n):
+        for j in range(n):
+            for k2 in range(n):
+                for l in range(n):
+                    i_value = 0
+                    for i in range(n):
+                        i_value += TransformationMatrix[i][i2] * temp1[i][j][k2][l]
+                    temp2[i2][j][k2][l] = i_value
+                    
+    temp3 = np.zeros(shape=(n, n, n, n))
+    for i2 in range(n):
+        for j in range(n):
+            for k2 in range(n):
+                for l2 in range(n):
+                    l_value = 0
+                    for l in range(n):
+                        l_value += TransformationMatrix[l][l2] * temp2[i2][j][k2][l]
+                    temp3[i2][j][k2][l2] = l_value
+                    
+    new_rdm2 = np.zeros(shape=(n, n, n, n))
+    for i2 in range(n):
+        for j2 in range(n):
+            for k2 in range(n):
+                for l2 in range(n):
+                    j_value = 0
+                    for j in range(n):
+                        j_value += TransformationMatrix[j][j2] * temp3[i2][j][k2][l2]
+                    new_rdm2[i2][j2][k2][l2] = j_value
+                    
+    return new_rdm1, new_rdm2
+
+
+world = madpy.MadWorld3D(thresh=1e-4)
+
+distance_list = [30.1631+0.025 * i for i in range(1)]
 Energy_list=[]
 Gradient_list=[]
 
@@ -14,9 +78,6 @@ for distance in distance_list:
     true_start = time.time()
     geometry = "Li 0.0 0.0 " + str(-distance/2) + "\nH 0.0 0.0 " + str(distance/2)
 
-    molecule=madpy.MadMolecule()
-    molecule.add_atom(0.0,0.0,0.0,"H")
-    molecule.add_atom(0.0,0.0,distance,"H")
     pno_start = time.time()
     madpno = madpy.MadPNO(world, geometry, n_orbitals=3)
     orbitals = madpno.get_orbitals()
@@ -26,8 +87,7 @@ for distance in distance_list:
 
     integrals = madpy.Integrals3D(world)
     orbitals = integrals.orthonormalize(orbitals=orbitals)
-    for orb in orbitals:
-        print(orb.type)
+    
 
     c = nuc_repulsion
     current=0.0
@@ -39,28 +99,37 @@ for distance in distance_list:
             T = integrals.compute_kinetic_integrals(orbitals)
             V = integrals.compute_potential_integrals(orbitals, Vnuc)
             S = integrals.compute_overlap_integrals(orbitals)
-            print(G.shape)
+            print("h1:",T+V)
+            print("g2:",G.elems)
+            print("s:",S)
             mol = tq.Molecule(geometry, one_body_integrals=T + V, two_body_integrals=G, nuclear_repulsion=c)
-            print(mol.n_electrons)
             params0=mol.parameters
-            print(params0.n_electrons)
         else:
             G = integrals.compute_two_body_integrals(active_orbitals)
             T = integrals.compute_kinetic_integrals(active_orbitals)
             V = integrals.compute_potential_integrals(active_orbitals, Vnuc)
             S = integrals.compute_overlap_integrals(active_orbitals)
-            print(G.shape)
+            print("h1:",T+V)
+            print("g2:",G.elems)
             params0.frozen_core=False
             mol = tq.Molecule(parameters=params0, one_body_integrals=T + V, two_body_integrals=G, nuclear_repulsion=c, 
                               n_electrons=2, active_orbitals=[0,1], frozen_orbitals=[])
-            print(mol.n_electrons)
-            print(mol.parameters.frozen_core)
-        
-        U = mol.make_ansatz(name="UpCCGD")
-        H = mol.make_hamiltonian()
+            
+        U = mol.make_ansatz(name="HCB-UpCCGD")
+        if iteration==0:
+            opt = get_best_initial_values(mol)
+        else:
+            opt = tq.quantumchemistry.optimize_orbitals(molecule=mol, circuit=U, silent=True, use_hcb=True, initial_guess=opt.mo_coeff)
+        mol_new=opt.molecule
+        H = mol_new.make_hardcore_boson_hamiltonian()
+        U = mol_new.make_ansatz(name="HCB-UpCCGD")
         E = tq.ExpectationValue(H=H, U=U)
-        result = tq.minimize(E, silent=True)
-        rdm1, rdm2 = mol.compute_rdms(U, variables=result.variables)
+        result = tq.minimize(E, silent=True, use_hcb=True)
+        rdm1, rdm2 = mol.compute_rdms(U=U, variables=result.variables, use_hcb=True)
+        rdm1, rdm2 = transform_rdms(opt.mo_coeff.transpose(), rdm1, rdm2)
+    
+        print("1-RDM:", rdm1)
+        print("2-RDM:", rdm2)
 
         print("iteration {} energy {:+2.7f}".format(iteration, result.energy))
         if abs(result.energy-current)<1e-6:
@@ -79,18 +148,7 @@ for distance in distance_list:
         for i in range(len(orbitals)):
             world.line_plot(f"orb{i}.dat", orbitals[i])
     Energy_list.append(result.energy)
-    #gradient calculation
-    part_deriv_V = molecule.compute_nuclear_derivative(world, 1, 2)
-    Deriv_tens = integrals.compute_potential_integrals(orbitals, part_deriv_V)
-    part_deriv_c = molecule.nuclear_repulsion_derivative(1, 2)
-
-    grad=0.0
-    for i in range(len(orbitals)-1):
-        for j in range(len(orbitals)-1):
-            grad += rdm1[i, j] * Deriv_tens[i, j]
-
-    print("gradient: ", grad+part_deriv_c)
-    Gradient_list.append(grad+part_deriv_c)
+    
     
     true_end = time.time()
     print("Total time: ", true_end - true_start)
