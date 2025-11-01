350e4a61b5fd32d94387deae2163b3274c747bd8
Thuy <thuytruong112@gmail.com>
2025-11-01 15:01:03 +0100

    fci_benchmark

---
+++ examples/excited_states/plot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path = "/Users/truonthu/Documents/MRA-FrayedEnds/FrayedEnds/examples/excited_states/fci_results.dat"
+
+data = np.loadtxt(file_path, comments="#")
+
+distance = data[:, 0]
+fci = data[:, 1]
+
+# Plot
+plt.figure(figsize=(8,5), dpi=300)
+plt.plot(distance, fci, label="FCI")
+plt.xlabel("internuclear distance (Å)")
+plt.ylabel("potential energy (eV)")
+plt.grid(True)
+plt.legend(loc="best")
+plt.xlim(0, 5.5)
+#plt.ylim(-1.2, -0.9)  # optional
+plt.tight_layout()
+plt.show()
+
