4d2d99d1c1930379c7f83241ce7a77eb40db7454
Thuy <thuytruong112@gmail.com>
2025-12-12 12:10:54 +0100

    updated names for plots

--- examples/excited_states/h4_fci_fixedbasis.py
+++ examples/excited_states/h4_fci_fixedbasis.py
@@ -1,13 +1,13 @@
 import tequila as tq
 import numpy as np
 
-geometry_mode = "equidistant"  # "equidistant" or "h2_pair"
+geometry_mode = "h2_pair"  # "equidistant" or "h2_pair"
 print(f"Geometry mode: {geometry_mode}")
 
 if geometry_mode == "equidistant":
     distance = np.arange(2.5, 0.45, -0.05).tolist()  # for linear H4 molecule with equidistant spacing d
 elif geometry_mode == "h2_pair":
-    distance = np.arange(1.5, 0.25, -0.02).tolist() # for H2 pair getting closer
+    distance = np.arange(1.5, 0.2, -0.02).tolist() # for H2 pair getting closer
 else:
     raise ValueError("Invalid geometry mode selected.")
 
