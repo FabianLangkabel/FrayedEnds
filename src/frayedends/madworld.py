2bb6fdb24d5eaaa5d2c8bba62e307c6f7cfae014
J. S. Kottmann <jakob.kottmann@gmail.com>
2026-04-12 06:37:34 +0200

    added tests for evaluate

--- src/frayedends/madworld.py
+++ src/frayedends/madworld.py
@@ -258,9 +258,14 @@ class MadWorld2D:
         else:
             self.impl.plane_plot(filename, mra_function, plane, zoom, datapoints, origin)
 
-    def evaluate(self, orbitals, points, units=None):
-        if units is None:
+    def evaluate(self, functions, points, units=None, silent=False, *args, **kwargs):
+        # convenience
+        nolist = not isinstance(functions, list)
+        if units is None and not silent:
             print("evaluate: no units given, default is Bohr!!!")
         if hasattr(units, "lower") and units.lower() == "angstrom":
             points = [x * 1.8897259886 for x in points]
-        return self.impl.evaluate(orbitals, points)
+        if nolist:
+            return self.impl.evaluate([functions], points)[0]
+        else:
+            return self.impl.evaluate(functions, points)
