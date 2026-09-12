d8c863ffc5c2b638f83629e1a5d2fe90a814195d
Thuy <thuytruong112@gmail.com>
2026-09-12 11:12:53 +0200

    changed distance

--- examples/excited_states/cispd_pnos/spa_h4_distance.py
+++ examples/excited_states/cispd_pnos/spa_h4_distance.py
@@ -12,7 +12,7 @@ madness_thresh = 1.0e-6
 econv = 1.0e-6
 
 
-distance = np.round(np.arange(3.0, 0.55, -0.05, dtype=np.float64), 3).tolist()
+distance = np.round(np.arange(2.5, 0.55, -0.05, dtype=np.float64), 3).tolist()
 
 with open("spa_h4.dat", "w") as f:
     header = "distance dist_time_s fci_energy_0 spa_energy_0 fci_energy_1 spa_energy_1"
